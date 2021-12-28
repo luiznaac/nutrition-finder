235b6853ada7b897ccca91d06aa02b642d36952f
Luiz Agner <luizagnern@hotmail.com>
2021-12-28 20:41:10 -0300

    Initializing project

---
+++ src/main/kotlin/com/monstro/nutritionfinder/NutritionFinderApplication.kt
@@ -0,0 +1,11 @@
+package com.monstro.nutritionfinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NutritionFinderApplication
+
+fun main(args: Array<String>) {
+	runApplication<NutritionFinderApplication>(*args)
+}
---
+++ src/test/kotlin/com/monstro/nutritionfinder/NutritionFinderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.monstro.nutritionfinder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NutritionFinderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
