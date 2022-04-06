b1503d8863e116c1a26ee2f050efd9ad9c846bd1
Luiz Agner <luizagnern@hotmail.com>
2022-04-06 20:25:13 -0300

    Setting up kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("io.kotest") version "0.3.9"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 }
@@ -23,6 +24,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-assertions-core:5.0.1")
+	testImplementation("io.kotest:kotest-framework-engine-jvm:5.0.1")
+	testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+	testImplementation("io.kotest:kotest-runner-junit5:5.0.1")
 }
 
 tasks.withType<KotlinCompile> {
